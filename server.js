bb214e05790bf2a0e665a6235137fc08dcc0e8f2
not-soo-cool <yashvendrasengar1111@gmail.com>
2023-09-14 01:52:02 +0530

    Okay

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const http = require('ht')
 const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
 
@@ -9,15 +10,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
-
-// import {v2 as cloudinary} from 'cloudinary';
-          
-// cloudinary.config({ 
-//   cloud_name: 'dcq3gtqtn', 
-//   api_key: '997367811343619', 
-//   api_secret: 'LpF8WQaCtheG-0GCb8tDawFN3x4' 
-// });
-
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`)
